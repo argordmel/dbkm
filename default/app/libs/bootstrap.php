5b3ccbea4d1b072404d254cad37e5422b93c0f92
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 23:28:14 +0800

    Delete default directory

--- default/app/libs/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Bootstrap de la aplicacion para personalizarlo
-// Para cargar cambia en public/index.php el require del bootstrap a app
-
-// Arranca KumbiaPHP
-require_once CORE_PATH . 'kumbia/bootstrap.php';
-
