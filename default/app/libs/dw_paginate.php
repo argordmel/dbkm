44ad8c76e34fd7e0c5cfb0132e6afbf65889c2ce
Iván Meléndez <argordmel@yahoo.es>
2025-05-21 16:25:21 -0500

    [fixed] Supports php 8

--- default/app/libs/dw_paginate.php
+++ default/app/libs/dw_paginate.php
@@ -23,19 +23,19 @@ class DwPaginate {
      * per_page: cantidad de elementos por pagina <br>
      *
      *
-     * @param array $model
-     * @return stdClass 
+     * @param object $model
+     * @return stdClass
      */
     public static function paginate($model) {
-                
-        $params = Util::getParams(func_get_args($model));
-        $page_number = isset($params['page']) ? Filter::get($params['page'], 'numeric') : 1; //Numero de la página       
-        $per_page = isset($params['per_page']) ? Filter::get($params['per_page'], 'numeric') : DATAGRID; //Datos por página        
-        $counter = ($page_number > 1) ? ( ($page_number * $per_page) - ($per_page-1) ) : 1; //Determino el contador para utilizarlo en la vista       
-        
-        $start = $per_page * ($page_number - 1); //Determino el offset      
-        $page = new stdClass(); //Instancia del objeto contenedor de pagina        
-        
+
+        $params = Util::getParams(func_get_args());
+        $page_number = isset($params['page']) ? (int) $params['page'] : 1;
+        $per_page = isset($params['per_page']) ? (int) $params['per_page'] : DATAGRID; //Datos por página
+        $counter = ($page_number > 1) ? ( ($page_number * $per_page) - ($per_page-1) ) : 1; //Determino el contador para utilizarlo en la vista
+
+        $start = $per_page * ($page_number - 1); //Determino el offset
+        $page = new stdClass(); //Instancia del objeto contenedor de pagina
+
         //Si es un array, se hace páginacion de array
         if (is_array($model)) {
             $items = $model;
@@ -51,50 +51,50 @@ class DwPaginate {
             $total_items = $n;
             $page->items = array_slice($items, $start, $per_page);
         } else {
-            $find_args = array(); //Arreglo que contiene los argumentos para el find        
+            $find_args = array(); //Arreglo que contiene los argumentos para el find
             $conditions = null;
-             //Asignando parametros de busqueda        
-            if (isset($params['conditions'])) {            
+             //Asignando parametros de busqueda
+            if (isset($params['conditions'])) {
                 $conditions = $params['conditions'];
-            } else if (isset($params[1])) {         
+            } else if (isset($params[1])) {
                 $conditions = $params[1];
             }
-            if (isset($params['columns'])) {            
+            if (isset($params['columns'])) {
                 $find_args[] = "columns: {$params['columns']}";
             }
-            if (isset($params['join'])) {            
+            if (isset($params['join'])) {
                 $find_args[] = "join: {$params['join']}";
             }
-            if (isset($params['group'])) {            
+            if (isset($params['group'])) {
                 $find_args[] = "group: {$params['group']}";
             }
-            if (isset($params['having'])) {            
+            if (isset($params['having'])) {
                 $find_args[] = "having: {$params['having']}";
             }
-            if (isset($params['order'])) {            
+            if (isset($params['order'])) {
                 $find_args[] = "order: {$params['order']}";
             }
-            if (isset($params['distinct'])) {            
+            if (isset($params['distinct'])) {
                 $find_args[] = "distinct: {$params['distinct']}";
             }
-            
+
             //Count by paginated
             $find_args[] = "paginated: ".true;
-            
-            if (isset($conditions)) {            
+
+            if (isset($conditions)) {
                 $find_args[] = $conditions;
             }
             $total_items = call_user_func_array(array($model , 'count'), $find_args); //Se cuentan los registros
             $find_args[] = "offset: $start"; //Asignamos el offset
-            $find_args[] = "limit: $per_page"; //Asignamos el limit               
-            $page->items = call_user_func_array(array($model , 'find'), $find_args); //Se efectua la busqueda            
+            $find_args[] = "limit: $per_page"; //Asignamos el limit
+            $page->items = call_user_func_array(array($model , 'find'), $find_args); //Se efectua la busqueda
         }
-        
+
         //Se efectuan los cálculos para las paginas
         $page->next = ($start + $per_page) < $total_items ? ($page_number + 1) : false;
         $page->prev = ($page_number > 1) ? ($page_number - 1) : false;
         $page->current = $page_number;
-        $page->total_page = ceil($total_items / $per_page);        
+        $page->total_page = ceil($total_items / $per_page);
         if( ($page->total_page < $page_number) && ($total_items > 0)){
             $page->prev = false;
             $url = Router::get('route');
@@ -105,8 +105,8 @@ class DwPaginate {
         $page->counter = ($total_items >= $counter) ? $counter : 1;
         $page->size = $total_items;
         $page->per_page = $per_page;
-        
-        return $page;        
+
+        return $page;
     }
 
     /**
@@ -123,15 +123,15 @@ class DwPaginate {
      * per_page: cantidad de elementos por pagina <br>
      *
      *
-     * @param string $model modelo
+     * @param object $model modelo
      * @param string $sql consulta sql
      * @return stdClass
      */
-    public static function paginate_by_sql($model, $sql) {
+    public static function paginate_by_sql(ActiveRecord $model, $sql) {
 
         $params = Util::getParams(func_get_args());
-        $page_number = isset($params['page']) ? Filter::get($params['page'], 'numeric') : 1; //Numero de la página
-        $per_page = isset($params['per_page']) ? Filter::get($params['per_page'], 'numeric') : DATAGRID; //Datos por página
+        $page_number = isset($params['page']) ? (int) $params['page'] : 1;
+        $per_page = isset($params['per_page']) ? (int) $params['per_page'] : DATAGRID; //Datos por página
         $counter = ($page_number > 1) ? ( ($page_number * $per_page) - ($per_page-1) ) : 1; //Determino el contador para utilizarlo en la vista
 
         $start = $per_page * ($page_number - 1); //Determino el offset
@@ -144,8 +144,8 @@ class DwPaginate {
         $page->next = ($start + $per_page) < $total_items ? ($page_number + 1) : false;
         $page->prev = ($page_number > 1) ? ($page_number - 1) : false;
         $page->current = $page_number;
-        $page->total_page = ceil($total_items / $per_page);        
-        if( ($page->total_page < $page_number) && ($total_items > 0)){            
+        $page->total_page = ceil($total_items / $per_page);
+        if( ($page->total_page < $page_number) && ($total_items > 0)){
             $page->prev = false;
             $url = Router::get('route');
             $url = explode('pag', $url);
@@ -159,4 +159,3 @@ class DwPaginate {
         return $page;
     }
 }
-?>
